d2a74ca68d7339e576080a21493f7a19ed9f21cc
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-13 17:00:59 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,42 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeons_array = []
+  pigeons = {}
+  i = 0
+
+  data[:color].each do |key, val|
+    pigeons_array.push(val)
+  end
+  pigeons_array.flatten!.uniq!
+
+  while i < pigeons_array.length
+    pigeons[pigeons_array[i]] = {color: [], gender: [], lives: []}
+    i += 1
+  end
+
+  data[:color].each do |key, val|
+    pigeons.each do |pigeon, attributes|
+      if val.include?(pigeon)
+        pigeons[pigeon][:color].push(key.to_s)
+      end
+    end
+  end
+
+  data[:gender].each do |key, val|
+    pigeons.each do |pigeon, attributes|
+      if val.include?(pigeon)
+        pigeons[pigeon][:gender].push(key.to_s)
+      end
+    end
+  end
+
+  data[:lives].each do |key, val|
+    pigeons.each do |pigeon, attributes|
+      if val.include?(pigeon)
+        pigeons[pigeon][:lives].push(key.to_s)
+      end
+    end
+  end
+  pigeons
 end
