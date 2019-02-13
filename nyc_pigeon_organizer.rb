4ab147329f34078dd749ff8b0cac2739a4518377
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-13 16:38:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,11 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
-
+  pigeons_array = []
   pigeons = {}
 
   data[:color].each do |key, val|
-    binding.pry
+    pigeons_array.push(val)
   end
+  binding.pry
 end
