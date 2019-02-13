1f844b378fa0dc6f650e0c7c20eb91127a325e63
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-13 16:42:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,10 +3,16 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   pigeons_array = []
   pigeons = {}
+  i = 0
 
   data[:color].each do |key, val|
     pigeons_array.push(val)
   end
   pigeons_array.flatten!.uniq!
-  binding.pry
+
+  while i < pigeons_array.length
+    pigeons[pigeons_array[i]] = {}
+    i += 1
+  end 
+  
 end
